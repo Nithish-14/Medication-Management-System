62cc9cf076f12fcda489662147bb16e9dcff214c
Nithish <nithishm612345@gmail.com>
2025-06-21 22:33:09 +0530

    tsconfig update

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,4 +1,8 @@
-export const requireAuth = (req, res, next) => {
+import {Response, NextFunction} from 'express'
+
+import { AuthRequest } from '../middleware/authMiddleware';
+
+export const requireAuth = (req: AuthRequest, res: Response, next: NextFunction) => {
   if (!req.user) return res.status(401).json({ message: "Unauthorized" });
   next();
 };
--- backend/src/utils/getStats.ts
+++ backend/src/utils/getStats.ts
@@ -1,4 +1,8 @@
-export const getStats = (req, res) => {
+import {Response} from 'express'
+
+import { AuthRequest } from '../middleware/authMiddleware';
+
+export const getStats = (req: AuthRequest, res: Response) => {
   const userId = req.user?.id;
   if (!userId) {
     return res.status(401).json({ message: "Unauthorized" });
